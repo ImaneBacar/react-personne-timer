ac96be76cd7c689dcf58a3034e0e20ce0d91c409
BacarImane <imanebacar@outlook.fr>
2025-10-30 22:04:44 +0000

    utilisation des useState et useEffect

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+
+
+import { useEffect, useState } from 'react';
+
+class Personne {
+  constructor({ fullName, bio, imgSrc, profession, show }) {
+    this.fullName = fullName;
+    this.bio = bio;
+    this.imgSrc = imgSrc;
+    this.profession = profession;
+    this.show = show;
+  }
+
+  toString() {
+    return `Nom : ${this.fullName}, Bio : ${this.bio}`;
+  }
+}
 
 function App() {
+  const [show, setShow] = useState(false);
+  const [timer,setTimer]= useState(0);
+
+  function handleClick(){
+      setShow(!show);
+  }
+
+  const personne1 = new Personne({
+    fullName: "John Doe",
+    bio: "Développeur passionné",
+    imgSrc: "https://images.unsplash.com/photo-1508921912186-1d1a45ebb3c1?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cGhvdG98ZW58MHx8MHx8fDA%3D&fm=jpg&q=60&w=3000",
+    profession: "Ingénieur",
+    show: show
+  });
+
+  useEffect(()=>{
+    let intervalId;
+    if(show){
+      setTimer(0);
+      intervalId = setInterval(() => {
+        setTimer(timer => timer+1);
+      }, 1000);
+    }  
+
+    return ()=> {clearInterval(intervalId); };
+  },[show]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <button onClick={handleClick}>{show ? " Masquer " : "Afficher"}</button>
+        { 
+          show && ( 
+          <div>
+          <h2>{personne1.fullName}</h2>
+          <p>{personne1.bio}</p>
+          <p>{personne1.profession}</p>
+          <img src={personne1.imgSrc} alt={personne1.fullName}  style={{width : "300px"}}/>
+          <p>Temps en second : {timer}</p>
+          </div>
+          )}
+
     </div>
   );
 }
